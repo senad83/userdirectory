66e6bb9656e2a6798a816751b281960f31670b56
Senad <senadutah@gmail.com>
2020-08-18 20:27:16 -0600

    heroku server

---
+++ build/precache-manifest.a7fa96246c3ea8cdef33b19db30e3dd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487be62d17604dc27d536dcb6b6d994e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5b2168dff51795e12fc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c297b18578962ffbd46d",
+    "url": "/static/js/2.197463c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.197463c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b2168dff51795e12fc",
+    "url": "/static/js/main.b68b9671.chunk.js"
+  },
+  {
+    "revision": "4575fedeb4cd5f0e221d",
+    "url": "/static/js/runtime-main.b01862d8.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-let express = require("express");
+
+const express = require ("express");
+// create port on 3001 or other available one
+let app = express();
+let port = process.env.PORT || 3001; 
+
+// creating static route that will run into a build
+app.use (express.static(__dirname + "/build"))
+
+// * means everything (it alway redirects on index.html)
+app.get ("*", function(req, res, next){
+    res.sendFile(__dirname + "/build/index.html")
+})
+
+// app is listening on port from line 4
+app.listen (port, function(){
+    console.log ("server is running", port)
+})
 
 // create express server 
 // build react app, npm run build -pck json
